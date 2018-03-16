cb1c01eb10eb3a9e9b25bec5690d1795858c1486
Ryan Manchester <ryanm17@gmail.com>
2018-03-16 20:06:51 +0000

    added 'fake' code to ensure load path works

--- album_reviews/lib/album_reviews.rb
+++ album_reviews/lib/album_reviews.rb
@@ -1,6 +1,6 @@
-require 'album_reviews/version'
-require 'album_reviews/cli'
-
-module AlbumReviews
-  # Your code goes here...
-end
+# <<<<<<< HEAD
+require_relative 'album_reviews/version'
+require_relative 'album_reviews/cli'
+# =======
+#require_relative "album_reviews/version"
+# >>>>>>> d5524c1e764ffc36aabdf7d0a5fd8cf2cc98ee4a
--- album_reviews/lib/album_reviews/cli.rb
+++ album_reviews/lib/album_reviews/cli.rb
@@ -1,10 +1,35 @@
 require 'pry'
+#puts "Reading cli.rb"
 
 class AlbumReviews::CLI
 
   def call
     puts "This week's album reviews:"
+    list_reviews
+    menu
+  end
 
+  def list_reviews
+    puts <<~END
+    1. Album of the Week: Mount Eerie
+    2. Other Reviews: Yo La Tango
+    END
+  end
+
+  def menu
+    puts "Which review would you like to read (1-2)? Or type 'exit' to leave."
+    input = gets.strip
+    while input != 'exit'
+      input 
+      case input
+      when "1"
+        puts "Mount Eerie is sad music."
+      when "2"
+        puts "Yo La Tango is not sad music."
+      else
+        "Enter the number of the review or enter 'exit'."
+      end
+    end
   end
 
 end
